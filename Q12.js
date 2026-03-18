c3872ff06d5fff8ce24add9169fcc504b5ad1ec2
Shivangi <shiivangii.gupta@gmail.com>
2026-03-18 13:56:08 +0530

    Q12 done

---
+++ Q12.js
@@ -0,0 +1,10 @@
+// Accept two numbers and print the greatest between them
+
+function greatest(a,b){
+    if(a>b){
+        return a
+    }else{
+        return b
+    }
+}
+console.log(greatest(13,7))

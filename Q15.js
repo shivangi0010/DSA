c7674e2c56d22991fdcc43d9b2b9704db1583685
Shivangi <shiivangii.gupta@gmail.com>
2026-03-18 14:06:48 +0530

    Q15 done

---
+++ Q15.js
@@ -0,0 +1,10 @@
+// Accept an integer and check whether it is an even number or odd.
+
+function evenOdd(n){
+    if(n%2==0){
+        console.log(n,"is an even number.")
+    }else{
+        console.log(n,"is an odd number.")
+    }
+}
+evenOdd(4)

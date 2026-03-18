7ccb5b18526dbae733ce741f8e50629deaae5e70
Shivangi <shiivangii.gupta@gmail.com>
2026-03-18 13:13:52 +0530

    Q4 done

---
+++ Q4.js
@@ -0,0 +1,6 @@
+// Accept the User's name, age and print in following manner (Ex - Hello Shery, you are 12 years old.)
+
+function intro(name,age){
+    console.log("Hello",name,", you are",age,"years old.")
+}
+intro("Shery" , 12)

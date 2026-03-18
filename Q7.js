d2740448f68d9615b6477c92dd11783ed80584ae
Shivangi <shiivangii.gupta@gmail.com>
2026-03-18 13:27:13 +0530

    Q7 done

---
+++ Q7.js
@@ -0,0 +1,7 @@
+// Accept the length and width of a rectangle. Calculate & print the area and perimeter.
+
+function rectangle(length,width){
+    console.log("Area: ",length*width)
+    console.log("Perimeter: ",2*length*width)
+}
+rectangle(4,6)

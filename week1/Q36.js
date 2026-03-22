dec48c5cb0787f4ea27d6e1787e7ceefc59174aa
Shivangi <shiivangii.gupta@gmail.com>
2026-03-22 22:00:29 +0530

    Fix output message format in reverseNumber function

--- week1/Q36.js
+++ week1/Q36.js
@@ -2,10 +2,11 @@
 
 function reverseNumber(n){
     let reverse = 0
+    let num = n
     for(; n>0 ; n=Math.floor(n/10)){
         let rem = n%10
         reverse = reverse*10 +rem
     }
-    console.log("The reverse of the number is:",reverse)
+    console.log("The reverse of the number",num, "is:",reverse)
 }
 reverseNumber(123)

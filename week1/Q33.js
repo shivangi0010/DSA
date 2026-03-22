dbf9797f42a9c5b0daefb1369e4bad7901cde83e
Shivangi <shiivangii.gupta@gmail.com>
2026-03-22 20:35:26 +0530

    Q33 done

---
+++ week1/Q33.js
@@ -0,0 +1,6 @@
+// Write a program to take two inputs a, b & find the value of a raised to the power of b. (Ex - a = 2, b = 5, OP - 2^5 = 32)
+
+function power(a,b){
+    return Math.pow(a , b)
+}
+console.log(power(2 , 5))

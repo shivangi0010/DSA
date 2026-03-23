6ee99498d1f3cab0b252b199fff453878935fd0f
Shivangi <shiivangii.gupta@gmail.com>
2026-03-23 18:16:48 +0530

    Add function to check if a character is a vowel or consonant using switch statement

---
+++ week1/Q43.js
@@ -0,0 +1,24 @@
+// Question 22 using Switch (Check Consonant or Vowel using Switch)
+
+function checkVowel(alpha){
+    switch(alpha){
+        case "a":
+            console.log(alpha,"is a vowel")
+            break
+        case "e":
+            console.log(alpha,  "is a vowel")
+            break
+        case "i":
+            console.log(alpha, "is a vowel")
+            break
+        case "o":
+            console.log(alpha, "is a vowel")
+            break
+        case "u":
+            console.log(alpha, "is a vowel")
+            break
+        default:
+            console.log(alpha, "is a consonant")
+    }
+}
+checkVowel("e")

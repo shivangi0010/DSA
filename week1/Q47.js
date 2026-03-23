670a968e0e65350c4b2b98d6e2ac5d43c9086bd8
Shivangi <shiivangii.gupta@gmail.com>
2026-03-23 18:56:58 +0530

    Add right triangle number pattern implementation

---
+++ week1/Q47.js
@@ -0,0 +1,12 @@
+// Right Triangle - Number Pattern
+
+function numberTriangle(n){
+    for(let i =1; i<= n ;i++){
+        let row = ""
+        for(let j = 1; j<= i; j++){
+            row = row + j + " "
+        }
+        console.log(row)
+    }
+}
+numberTriangle(5)

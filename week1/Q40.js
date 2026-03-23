0ac1c515b89d5a447a9db8fe67c6d945ba5054c6
Shivangi <shiivangii.gupta@gmail.com>
2026-03-23 10:44:17 +0530

    Add function to print "Hello" until user provides incorrect input

---
+++ week1/Q40.js
@@ -0,0 +1,8 @@
+// Print \hello\" until user gives wrong input using do while"
+
+function printHello(input){
+    do{
+        console.log("Hello")
+    }while(input !== "correct")
+}
+printHello("correct")

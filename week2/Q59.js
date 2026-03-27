d328e4ba14d653e2186214cca00f8bbdc7813668
Shivangi <shiivangii.gupta@gmail.com>
2026-03-27 11:08:22 +0530

    Refactor array left rotation implementation for improved readability

--- week2/Q59.js
+++ week2/Q59.js
@@ -1,11 +1,10 @@
 // Array left Rotation by 1
 
-let arr = [10,20,30,40,50]
+let arr = [10 , 20 ,30 , 40 , 50]
 let first = arr[0]
 
-for(let i = 0; i<arr.length ; i++){
+for(let i = 0 ; i<arr.length - 1 ; i++){
     arr[i] = arr[i+1]
 }
-arr[arr.length-1] = first
-
+arr[arr.length - 1] = first
 console.log(arr)

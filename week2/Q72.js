b98d6759821147e30a204ff1d0e976bc0a555327
Shivangi <shiivangii.gupta@gmail.com>
2026-03-28 00:34:27 +0530

    You are given two arrays of **equal length**: An **integer array** A[] and a **character array** B[], where each character is one of 'a', 'b', or 'c'. Find the **minimum** value of the expression: min(sum of values linked with 'a' + sum of values linked with 'b', sum of values linked with 'c').

---
+++ week2/Q72.js
@@ -0,0 +1,23 @@
+// You are given two arrays of **equal length**: An **integer array** A[] and a **character array** B[], where each character is one of 'a', 'b', or 'c'. Find the **minimum** value of the expression: min(sum of values linked with 'a' + sum of values linked with 'b', sum of values linked with 'c').
+
+
+let A = [2, 1, 3, 4, 5, 6]
+let B = ["a" , "b", "a", "c" , "b" ,"c" ]
+
+let sumA = 0
+let sumB = 0
+let sumC = 0
+
+for(let i = 0 ; i< A.length ; i++){
+        if(B[i] == "a"){
+            sumA += A[i]
+        }else if(B[i] == "b"){
+            sumB += A[i]
+        }else if(B[i] == "c"){
+            sumC += A[i]
+        }
+}
+let sumAB = sumA + sumB
+console.log("Sum of values of a's and b's :" ,sumAB)
+console.log("Sum of values of c's :", sumC)
+console.log("Minimum of the two:",Math.min(sumAB , sumC))

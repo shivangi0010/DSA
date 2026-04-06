c50c54f27f0d48e693edf1a0152f3a7ce4155ea9
Shivangi <shiivangii.gupta@gmail.com>
2026-04-06 10:11:53 +0530

    // Accept a string from user and print its each character on a new line

---
+++ week3/Q100.js
@@ -0,0 +1,8 @@
+// Accept a string from user and print its each character on a new line 
+
+function string(s){
+    for(let i = 0 ; i < s.length ; i++){
+        console.log(s.charAt(i))
+    }
+}
+string("hehe")

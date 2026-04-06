0a798d088b9de3ccb3052ab8b733654728b4d958
Shivangi <shiivangii.gupta@gmail.com>
2026-04-06 10:14:37 +0530

    // Sort the words of the sentence

---
+++ week3/Q112.js
@@ -0,0 +1,8 @@
+// Sort the words of the sentence 
+
+function sortWords(sent){
+    let arr = sent.split(" ")
+    let sortedArr = arr.sort()
+    console.log(sortedArr)
+}
+sortWords(" Hi this Os Shivangi")

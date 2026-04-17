10b776f59a76d2af802168d593456a30c0be2ff5
Shivangi <shiivangii.gupta@gmail.com>
2026-04-17 14:25:43 +0530

    Q136 done

---
+++ week3/Q136.js
@@ -0,0 +1,15 @@
+// Move Zeroes
+
+function moveZeroes(nums) {
+    let i = 0;
+
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++;
+        }
+    }
+
+    return nums;
+}
+console.log(moveZeroes([0,1,0,3,12]))

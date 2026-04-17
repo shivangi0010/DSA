26c27e35209e7074f5faf0661a1a182c2c5a4249
Shivangi <shiivangii.gupta@gmail.com>
2026-04-17 14:25:53 +0530

    Q137 done

---
+++ week3/Q137.js
@@ -0,0 +1,21 @@
+// Sort Array By Parity
+
+function sortArrayByParity(nums) {
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i < j) {
+        if (nums[i] % 2 === 0) {
+            i++;
+        } else if (nums[j] % 2 === 1) {
+            j--;
+        } else {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++;
+            j--;
+        }
+    }
+
+    return nums;
+}
+console.log(sortArrayByParity([3,1,2,4]))

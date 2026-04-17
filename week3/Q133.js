f7c37479e54a354225f3cfcdd9b0636923717421
Shivangi <shiivangii.gupta@gmail.com>
2026-04-17 14:24:41 +0530

    Q133 done

---
+++ week3/Q133.js
@@ -0,0 +1,19 @@
+// valid mountain array 
+
+function validMountainArray(arr) {
+    let n = arr.length;
+    let i = 0;
+
+    while (i < n - 1 && arr[i] < arr[i + 1]) {
+        i++;
+    }
+
+    if (i === 0 || i === n - 1) return false;
+
+    while (i < n - 1 && arr[i] > arr[i + 1]) {
+        i++;
+    }
+
+    return i === n - 1;
+}
+console.log(validMountainArray([0,1,3,2,1]))

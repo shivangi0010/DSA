2f7fdd886d84abadc62367aa61dd8965c97ca026
Shivangi <shiivangii.gupta@gmail.com>
2026-04-17 14:25:23 +0530

    Q134 done

---
+++ week3/Q134.js
@@ -0,0 +1,12 @@
+function replaceElements(arr) {
+    let max = -1;
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let temp = arr[i];
+        arr[i] = max;
+        max = Math.max(max, temp);
+    }
+
+    return arr;
+}
+console.log(replaceElements([17,18,5,4,6,1]))
